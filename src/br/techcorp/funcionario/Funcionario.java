e91a9a3393799e44cd7e12dd2e7ef5c6f3a56108
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 20:55:32 -0300

    Projeto TechCorp em andamento!

---
+++ src/br/techcorp/funcionario/Funcionario.java
@@ -0,0 +1,56 @@
+package br.techcorp.funcionario;
+
+public class Funcionario {
+    private String matricula;
+    private String nome;
+    private String cargo;
+    private double salarioBase;
+    private double percentualBonus;
+    private boolean ativo;
+
+    public Funcionario(String matricula, String nome, String cargo, double salarioBase) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+
+    public void definirBonus(double percentual){
+        if (percentual>0){
+            percentualBonus = percentual;
+        }
+    }
+    public void desligar(){
+        ativo = false;
+        percentualBonus = 0;
+    }
+
+    public boolean isAtivo (){
+        return ativo;
+    }
+
+    public void aplicarAumento(double percentual){
+        salarioBase *=(1 + percentual / 100);
+    }
+}
